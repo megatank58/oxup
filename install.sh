0497a1053b291b7cb9daa34f8cd050d9a6ae8063
megatank58 <tanumegatank@gmail.com>
2023-05-06 14:00:10 +0530

    feat(install.sh): add prompt

--- install.sh
+++ install.sh
@@ -17,5 +17,31 @@ else
     ./oxate.exe setup
 fi
 
-echo -e "${PREFIX}Add the following lines to your shell configuration to add oxate to PATH: \`. \"$HOME/.oxido/env\"\`"
+#!/bin/sh
+BLUE='\033[0;34m'
+NONE='\033[0m'
+PREFIX="${BLUE}=>${NONE} "
+echo -e "${PREFIX}Downloading oxate"
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    curl --proto '=https' --tlsv1.2 -sSfLO https://github.com/oxidic/oxate/releases/latest/download/oxate
+    chmod +x oxate
+    ./oxate setup
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    curl --proto '=https' --tlsv1.2 -sSfLO https://github.com/oxidic/oxate/releases/latest/download/oxate-darwin
+    chmod +x oxate-darwin
+    ./oxate-darwin setup
+else
+    curl --proto '=https' --tlsv1.2 -sSfLO https://github.com/oxidic/oxate/releases/latest/download/oxate.exe
+    ./oxate.exe setup
+fi
+
+read -p "${PREFIX}Add binary to path (y/n)" confirm && if [[ ! "$confirm" =~ ^[Yy]+([eE][sS])?$ ]]; then 
+    if [[ $SHELL == *"bash" ]]; then
+        echo '. "$HOME/.oxido/env"' >> $HOME/.bashrc
+    elif [[ $SHELL == *"zsh" ]]; then
+        echo '. "$HOME/.oxido/env"' >> $HOME/.zshrc
+    else
+        echo -e "${PREFIX}Write \`. \"$HOME/.oxido/env\"\` in the shell config to put oxate in path."
+    fi
+fi
