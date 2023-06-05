72ffaf6338a008d0984c5265cccd0946fa04bd08
megatank58 <tanumegatank@gmail.com>
2023-06-05 21:31:52 +0530

    fix: install script

--- install.sh
+++ install.sh
@@ -17,25 +17,6 @@ else
     ./oxate.exe setup
 fi
 
-#!/bin/sh
-BLUE='\033[0;34m'
-NONE='\033[0m'
-PREFIX="${BLUE}=>${NONE} "
-echo -e "${PREFIX}Downloading oxate"
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    curl --proto '=https' --tlsv1.2 -sSfLO https://github.com/oxidic/oxate/releases/latest/download/oxate
-    chmod +x oxate
-    ./oxate setup
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    curl --proto '=https' --tlsv1.2 -sSfLO https://github.com/oxidic/oxate/releases/latest/download/oxate-darwin
-    chmod +x oxate-darwin
-    ./oxate-darwin setup
-else
-    curl --proto '=https' --tlsv1.2 -sSfLO https://github.com/oxidic/oxate/releases/latest/download/oxate.exe
-    ./oxate.exe setup
-fi
-
 read -p "${PREFIX}Add binary to path (y/n)" confirm && if [[ ! "$confirm" =~ ^[Yy]+([eE][sS])?$ ]]; then 
     if [[ $SHELL == *"bash" ]]; then
         echo '. "$HOME/.oxido/env"' >> $HOME/.bashrc
