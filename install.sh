90fe5434dd7fa07560743ae1963348db6a9d08f8
megatank58 <tanumegatank@gmail.com>
2024-04-01 15:03:27 +0530

    chore: update install script

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 BLUE='\033[0;34m'
 NONE='\033[0m'
 PREFIX="${BLUE}=>${NONE} "
-echo -e "${PREFIX}Downloading oxate"
+echo "${PREFIX}Downloading oxate"
 
 curl --proto '=https' --tlsv1.2 -sSfLO https://github.com/oxidic/oxate/releases/latest/download/oxate
 chmod +x oxate
 ./oxate setup
 
-echo -e "${PREFIX}Write \`. \"$HOME/.oxido/env\"\` in the shell config to put oxate in path."
+echo "${PREFIX}Write \`. \"$HOME/.oxido/env\"\` in the shell config to put oxate in path."
